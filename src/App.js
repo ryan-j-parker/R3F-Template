e16d7a2634e35734ceddc7f3d002ad42a75aa3f7
Ryan J. Parker <ryan.jos.parker@gmail.com>
2023-01-28 21:38:20 -0800

    add basic template and all dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+/* eslint-disable react/no-unknown-property */
+import { Loader, OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import './App.css';
+import * as THREE from 'three';
+import { ACESFilmicToneMapping, sRGBEncoding } from 'three';
+import { Suspense } from 'react';
+import Box from './Box';
+import FloatingText from './FloatingText';
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<Loader />}>
+      <Canvas
+        shadows
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 400,
+          position: [0, 2, 10],
+        }}
+        gl={{
+          antialias: true,
+          toneMapping: ACESFilmicToneMapping,
+          outputEncoding: sRGBEncoding,
+        }}
+      >
+        <color attach="background" args={['#111']} />
+        <ambientLight intensity={3} />
+        <spotLight intensity={2} args={[0, 3, 4]} />
+        <pointLight position={[20, 10, -10]} intensity={2} />
+        <primitive object={new THREE.AxesHelper(2)} />
+        <primitive object={new THREE.GridHelper(20, 20)} />
+        <OrbitControls />
+        <FloatingText />
+        <Box key="box" />
+      </Canvas>
+    </Suspense>
   );
 }
-
-export default App;
